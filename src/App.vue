5e25c1ee2dd32884c5d913019282571ed7217057
Alexx Sub <alexei.s@list.ru>
2022-08-15 17:41:39 +0300

    reactive

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
+const name = ref("");
+</script>
+
+<template>
+  <input v-model="name" />
+  <HelloWorld :msg="`Hello, ${name}!`"></HelloWorld>
+</template>
