8b3a11fb265fc0cdc221683a4e0fcc5c47639b9e
Alexx Sub <alexei.s@list.ru>
2022-08-15 18:02:45 +0300

    vfor

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
 import { ref } from "vue";
-const name = ref("");
+const todos = ref([
+  "Сходить за хлебом",
+  "Купить воблы",
+  "Не забыть про пивасик",
+  "А! еще колбаски",
+]);
 </script>
 
 <template>
-  <input v-model="name" />
-  <HelloWorld :msg="`Hello, ${name}!`"></HelloWorld>
+  <div>
+    <h2>Выводим список дел</h2>
+    <br />
+    <ul>
+      <li v-for="todo in todos" :key="todo">{{ todo }}</li>
+      <!-- Перебираем массив todos и каждый элемент доступенв todo,
+      вы водим в шаблон-->
+    </ul>
+  </div>
 </template>
