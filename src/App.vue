832a3011213a485b5b76e72b0e42ff3f8cb8f024
Alexx Sub <alexei.s@list.ru>
2022-08-15 18:40:33 +0300

    addphone2

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup>
 import { ref } from "vue";
-const newPhone = ref("");
+const inputnumber = ref(null);
+const newPhone = ref({
+  number: "",
+  name: "",
+});
 //массив с данными, обычный массив
 const phones = ref([]);
 function addPhone() {
   //видимость переменных получаем через this
   phones.value.push(newPhone.value); //нам не надо заботится о выводе новых данных
   //как только элемент будет добавлен в массив, он появится в списке
-  newPhone.value = ""; // затираем переменную ввода
+  newPhone.value = { number: "", name: "" }; // затираем переменную ввода
+  inputnumber.value.focus();
 }
 </script>
 
@@ -15,13 +20,21 @@ function addPhone() {
   <div>
     <!-- Описываем поле вводы и вешаем обработчик на нажатие клавиши Enter -->
     <input
+      ref="inputnumber"
       placeholder="Введите номер"
-      v-model="newPhone"
+      v-model="newPhone.number"
+    />
+    <!--Добавляем еще одно поле ввода данных -->
+    <input
+      placeholder="Введите имя"
+      v-model="newPhone.name"
       @keyup.enter="addPhone"
     />
     <ul>
       <!-- Уже знакомый вывод списком-->
-      <li v-for="phone in phones" :key="phone">{{ phone }}</li>
+      <li v-for="phone in phones" :key="phone">
+        {{ phone.number }} {{ phone.name }}
+      </li>
     </ul>
   </div>
 </template>
